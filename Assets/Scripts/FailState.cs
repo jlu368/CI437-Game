81d9958046a7b26f6265690c65db0554f40440dc
Jeff L <j.lu368@gmail.com>
2017-05-06 13:05:09 -0500

    Moved things around.  Added game over and transitions

---
+++ Assets/Scripts/FailState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col){
+        Application.LoadLevel("Game Over");
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
